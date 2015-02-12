4a0f8786334c4363698904a744030b40408d5699
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-02-12 06:28:34 -0800

    tweaking popup.el

--- liquid-tip.el
+++ liquid-tip.el
@@ -1,6 +1,6 @@
 ;;; liquid-tip.el --- show inferred liquid-types
 
-;; Copyright (C) 2014 by Ranjit Jhala 
+;; Copyright (C) 2014 by Ranjit Jhala
 
 ;; Author: Ranjit Jhala <jhala@cs.ucsd.edu>
 ;; Version: 0.0.1
@@ -24,19 +24,11 @@
 
 ;;; Code:
 (eval-when-compile (require 'cl))
-(require 'auto-complete)
 (require 'json)
+(require 'popup)
 (require 'pos-tip nil t)
 (require 'thingatpt)
 (require 'button-lock)
-
-;; (require 'json-mode)
-;; (require 'ring)
-;; (require 'etags)
-;; (require 'flymake)
-;; (require 'eldoc)
-;; (require 'log4e)
-;; (require 'yaxception)
 
 ;; ------------------------------------------------------------------------
 ;; A structure to represent positions 
@@ -49,13 +41,13 @@
 ;; ------------------------------------------------------------------------
 
 (defun get-string-from-file (filePath)
-  "Return filePath's file content."
+  "Return FILEPATH's file content."
   (with-temp-buffer
     (insert-file-contents filePath)
     (buffer-string)))
 
 (defun get-json-from-file (filePath)
-  "Return json object from filePath's content"
+  "Return json object from FILEPATH content."
   (if (file-exists-p filePath)
       (let* ((json-key-type 'string)
 	     (str (get-string-from-file filePath)))
@@ -67,12 +59,13 @@
 ;; ------------------------------------------------------------------------
 
 (defun gethash-nil (key table) 
+  "Return the value of KEY from TABLE."
   (if table 
       (gethash key table nil)
       nil))
 
 (defun liquid-annot-filepath-prefix (mode)
-  "Return prefix of annotation file using mode"
+  "Return prefix of annotation file using MODE."
   (if (equal mode 'flycheck)
       "flycheck_"
       nil))
@@ -84,7 +77,7 @@
 ;;    ==> "/path/to/.liquid/file.hs.json"
 
 (defun liquid-annot-filepath (mode file)
-  "Return name of annotation file"
+  "Return MODE dependent name of annotation FILE."
   (let* ((dir    (file-name-directory file))
 	 (name   (file-name-nondirectory file))
 	 (prefix (liquid-annot-filepath-prefix mode)))
@@ -94,7 +87,7 @@
 
 ;; API
 (defun liquid-annot-set (file mode)
-  "Load information for file into liquid-annot-table"
+  "Load information for FILE (in current MODE) into liquid-annot-table."
   (let* ((file-path        (liquid-annot-filepath mode file))
 	 (json-object-type 'hash-table)
 	 (file-info        (get-json-from-file file-path)))
@@ -102,7 +95,7 @@
 
 ;; API
 (defun liquid-annot-get (file row col)
-  "Get annotation for identifier at row, col in file"
+  "Get annotation for identifier in FILE at ROW and COL." 
   (let* ((table (gethash-nil file liquid-annot-table))
 	 (r     (format "%d" row))
 	 (c     (format "%d" col))
@@ -123,26 +116,31 @@
 (defvar liquid-tip-mode 'balloon)
 
 (defun pad-line (str)
+  "Add extra blanks before and after STR."
   (concat " " str " "))
 
 (defun popup-tip-pad (text)
+  "Add extra blanks before and after TEXT."
   (let* ((lines     (split-string text "\n"))
          (pad-lines (mapcar 'pad-line lines))
 	 (pad-text  (concat "\n" (mapconcat 'identity pad-lines "\n") "\n")))
     (popup-tip pad-text)))
 
 (defun liquid-tip-popup-balloon (text)
-  "Display text in a balloon popup"
-  (if (and (functionp 'ac-quick-help-use-pos-tip-p)
-           (ac-quick-help-use-pos-tip-p))
-      (pos-tip-show text 'popup-tip-face nil nil 300 popup-tip-max-width)
-    (popup-tip-pad text)))
+  "Display TEXT in a balloon popup."
+  (popup-tip-pad text))
+
+  ;; (if (and (functionp 'ac-quick-help-use-pos-tip-p)
+  ;;          (ac-quick-help-use-pos-tip-p))
+  ;;     (pos-tip-show text 'popup-tip-face nil nil 300 popup-tip-max-width)
+  ;;   (popup-tip-pad text)))
 
 (defun liquid-tip-popup-ascii (text)
- "Display text in ascii popup"
+ "Display TEXT in ascii popup."
   (popup-tip-pad text))
 
 (defun liquid-tip-popup (text)
+  "Display TEXT."
   (if (equal liquid-tip-mode 'ascii)
       (liquid-tip-popup-ascii   text)
       (liquid-tip-popup-balloon text)))
@@ -153,14 +151,15 @@
   (rx (one-or-more (not (in " \n\t()[]{}")))))
 
 (defun liquid-splitters () 
+  "List of  identifier splitters."
   '( ?\s  ?\t ?\n ?\( ?\) ?\[ ?\] ))
 
-(defun liquid-is-split (c)
-  "Is the character `c` a splitter?"
-  (member c (liquid-splitters)))
+(defun liquid-is-split (char)
+  "Predicate to check if CHAR is a splitter?"
+  (member char (liquid-splitters)))
 
 (defun liquid-id-start-pos (low p)
-  "Find the largest position less than `p` that is a splitter"
+  "Find the largest position more than LOW but less than P that is a splitter."
   (let* ((ch (char-before p)))
      (if (or (<= p low) (liquid-is-split ch)) 
 	 p 
@@ -168,16 +167,17 @@
 
 
 (defun column-number-at-pos (pos)
-  "Find the column of position pos"
+  "Find the column of position POS."
   (+ (- pos (line-beginning-position)) 1))
 
 (defun start-column-number-at-pos (pos)
-  "Find the starting column of identifier at pos"
+  "Find the starting column of identifier at POS."
      (let* ((low   (line-beginning-position))
 	    (start (liquid-id-start-pos low pos)))
        (column-number-at-pos start)))
 
 (defsubst liquid-get-position ()
+  "Return the current cursor position."
   (save-excursion
     (widen)
     (make-position
@@ -186,7 +186,7 @@
      :col  (start-column-number-at-pos (point)))))
 
 (defun position-string (pos)
-  "position -> string"
+  "Return the current POS as a string."
   (format "(%s, %s) in [%s]" 
 	  (position-row pos) 
 	  (position-col pos)
@@ -207,18 +207,18 @@
 ;; DEBUG 	    ident)))
 
 (defun liquid-ident-at-pos (pos)
-  "Return the identifier at a given position"
+  "Return the identifier at POS."
   (thing-at-point 'word))
 
 (defun liquid-annot-at-pos-2 (pos)
-  "Info to display: type annotation for the identifier at the position or NONE" 
+  "Info to display: type annotation for the identifier at POS or NONE." 
   (let* ((file (position-file pos))
 	 (row  (position-row  pos))
 	 (col  (position-col  pos)))
     (liquid-annot-get file row col)))
 
 (defun liquid-annot-at-pos (pos)
-  "Determine info to display"
+  "Determine info to display at POS."
   (liquid-annot-at-pos-2 pos))
 
 ;;;###autoload
@@ -231,14 +231,14 @@
          (annot  (liquid-annot-at-pos pos)))
     (if annot 
 	(liquid-tip-popup annot)
-        (hdevtools/show-type-info)
-        ;; (liquid-tip-popup sorry)
+        ;; (hdevtools/show-type-info)
+        (liquid-tip-popup sorry)
 	)))
 
 
 ;;;###autoload
 (defun liquid-tip-init (&optional mode)
-  "Initialize liquid-tip by making all identifiers buttons"
+  "Initialize liquid-tip by making all identifiers buttons using MODE."
   (interactive)
   (progn (if mode (setq liquid-tip-mode mode))
 	 (button-lock-mode 1)
@@ -247,7 +247,7 @@
 
 ;;;###autoload
 (defun liquid-tip-update (mode)
-  "Update liquid-annot-table by reloading annot file for buffer"
+  "Update liquid-annot-table by reloading annot file for buffer in MODE."
   (interactive)
   (let* ((pos  (liquid-get-position))
 	 (file (position-file pos)))
