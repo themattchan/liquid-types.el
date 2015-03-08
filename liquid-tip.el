7a0f50249ca7d7679240d299b223b6379a365ef1
Matthew Chan <matt@themattchan.com>
2015-03-08 05:06:14 -0700

    popup error message if typechecking fails

--- liquid-tip.el
+++ liquid-tip.el
@@ -23,12 +23,15 @@
 ;; see https://github.com/ucsd-progsys/liquidhaskell#emacs
 
 ;;; Code:
+
+
 (eval-when-compile (require 'cl))
 (require 'json)
 (require 'popup)
 (require 'pos-tip nil t)
 (require 'thingatpt)
 (require 'button-lock)
+(require 'flycheck-liquid)
 
 ;; ------------------------------------------------------------------------
 ;; Defcustoms
@@ -41,15 +44,15 @@
 
 (defcustom liquid-tip-style 'ascii
   "Set popup style."
- :type '(choice (const :tag "Plain text" ascii)
-                (const :tag "Balloon" balloon))
- :group 'liquid-tip)
+  :type '(choice (const :tag "Plain text" ascii)
+                 (const :tag "Balloon" balloon))
+  :group 'liquid-tip)
 
 (defcustom liquid-tip-checker-name 'flycheck
   "Checker used by liquid, either 'flycheck or nil."
- :type '(choice (const :tag "Flycheck" flycheck)
-                (const :tag "nil" nil))
- :group 'liquid-tip)
+  :type '(choice (const :tag "Flycheck" flycheck)
+                 (const :tag "nil" nil))
+  :group 'liquid-tip)
 
 (defcustom liquid-tip-trigger 'S-double-mouse-1
   "Set trigger event for (liquid-tip-show).  Must be a valid Mouse button symbol."
@@ -79,9 +82,9 @@
   "Return json object from FILEPATH content."
   (if (file-exists-p filePath)
       (let* ((json-key-type 'string)
-         (str (get-string-from-file filePath)))
-    (json-read-from-string str))
-      nil))
+             (str (get-string-from-file filePath)))
+        (json-read-from-string str))
+    nil))
 
 ;; ------------------------------------------------------------------------
 ;; get/set annot information
@@ -91,13 +94,13 @@
   "Return the value of KEY from TABLE."
   (if table
       (gethash key table nil)
-      nil))
+    nil))
 
 (defun liquid-annot-filepath-prefix (mode)
   "Return prefix of annotation file using MODE."
   (if (equal mode 'flycheck)
       "flycheck_"
-      nil))
+    nil))
 
 ;; (liquid-annot 'flycheck "/path/to/file.hs")
 ;;    ==> "/path/to/.liquid/flycheck_file.hs.json"
@@ -108,8 +111,8 @@
 (defun liquid-annot-filepath (mode file)
   "Return MODE dependent name of annotation FILE."
   (let* ((dir    (file-name-directory file))
-     (name   (file-name-nondirectory file))
-     (prefix (liquid-annot-filepath-prefix mode)))
+         (name   (file-name-nondirectory file))
+         (prefix (liquid-annot-filepath-prefix mode)))
     (concat dir ".liquid/" prefix name ".json")))
 
 (defvar liquid-annot-table (make-hash-table :test 'equal))
@@ -118,19 +121,22 @@
 (defun liquid-annot-set (file mode)
   "Load information for FILE (in current MODE) into liquid-annot-table."
   (let* ((file-path        (liquid-annot-filepath mode file))
-     (json-object-type 'hash-table)
-     (file-info        (get-json-from-file file-path)))
+         (json-object-type 'hash-table)
+         (file-info        (get-json-from-file file-path)))
     (if file-info (puthash file file-info liquid-annot-table))))
 
 ;; API
 (defun liquid-annot-get (file row col)
   "Get annotation for identifier in FILE at ROW and COL."
   (let* ((table (gethash-nil file liquid-annot-table))
-     (r     (format "%d" row))
-     (c     (format "%d" col))
-     (tys   (gethash-nil "types" table))
-     (ro    (gethash-nil r tys)))
-    (gethash-nil "ann" (gethash-nil c ro))))
+         (r     (format "%d" row))
+         (c     (format "%d" col))
+         (err   (gethash-nil "errors" table))
+         (tys   (gethash-nil "types" table))
+         (ro    (gethash-nil r tys)))
+    (if err
+        (gethash-nil "message" (elt err 0))
+      (gethash-nil "ann" (gethash-nil c ro)))))
 
 ;; ------------------------------------------------------------------------
 ;; Display Annot in Tooltip
@@ -142,7 +148,7 @@
 ;; For emacs', balloon based popups, use:
 ;;    (setq liquid-tip-style 'balloon)
 
-(defvar liquid-tip-style 'balloon)
+;;(defvar liquid-tip-style 'balloon)
 
 (defun pad-line (str)
   "Add extra blanks before and after STR."
@@ -152,27 +158,27 @@
   "Add extra blanks before and after TEXT."
   (let* ((lines     (split-string text "\n"))
          (pad-lines (mapcar 'pad-line lines))
-     (pad-text  (concat "\n" (mapconcat 'identity pad-lines "\n") "\n")))
+         (pad-text  (concat "\n" (mapconcat 'identity pad-lines "\n") "\n")))
     (popup-tip pad-text)))
 
 (defun liquid-tip-popup-balloon (text)
   "Display TEXT in a balloon popup."
   (popup-tip-pad text))
 
-  ;; (if (and (functionp 'ac-quick-help-use-pos-tip-p)
-  ;;          (ac-quick-help-use-pos-tip-p))
-  ;;     (pos-tip-show text 'popup-tip-face nil nil 300 popup-tip-max-width)
-  ;;   (popup-tip-pad text)))
+;; (if (and (functionp 'ac-quick-help-use-pos-tip-p)
+;;          (ac-quick-help-use-pos-tip-p))
+;;     (pos-tip-show text 'popup-tip-face nil nil 300 popup-tip-max-width)
+;;   (popup-tip-pad text)))
 
 (defun liquid-tip-popup-ascii (text)
- "Display TEXT in ascii popup."
+  "Display TEXT in ascii popup."
   (popup-tip-pad text))
 
 (defun liquid-tip-popup (text)
   "Display TEXT."
   (if (equal liquid-tip-style 'ascii)
       (liquid-tip-popup-ascii   text)
-      (liquid-tip-popup-balloon text)))
+    (liquid-tip-popup-balloon text)))
 
 ;; -- Compute range ---------------------------------------------------------
 
@@ -190,9 +196,9 @@
 (defun liquid-id-start-pos (low p)
   "Find the largest position more than LOW but less than P that is a splitter."
   (let* ((ch (char-before p)))
-     (if (or (<= p low) (liquid-is-split ch))
-     p
-         (liquid-id-start-pos low (- p 1)))))
+    (if (or (<= p low) (liquid-is-split ch))
+        p
+      (liquid-id-start-pos low (- p 1)))))
 
 
 (defun column-number-at-pos (pos)
@@ -201,9 +207,9 @@
 
 (defun start-column-number-at-pos (pos)
   "Find the starting column of identifier at POS."
-     (let* ((low   (line-beginning-position))
-        (start (liquid-id-start-pos low pos)))
-       (column-number-at-pos start)))
+  (let* ((low   (line-beginning-position))
+         (start (liquid-id-start-pos low pos)))
+    (column-number-at-pos start)))
 
 (defsubst liquid-get-position ()
   "Return the current cursor position."
@@ -217,9 +223,9 @@
 (defun position-string (pos)
   "Return the current POS as a string."
   (format "(%s, %s) in [%s]"
-      (position-row pos)
-      (position-col pos)
-      (position-file pos)))
+          (position-row pos)
+          (position-col pos)
+          (position-file pos)))
 
 ;; DEBUG (defun liquid-annot-at-pos-0 (pos)
 ;; DEBUG   "Info to display: just the file/line/constant string"
@@ -242,8 +248,8 @@
 (defun liquid-annot-at-pos-2 (pos)
   "Info to display: type annotation for the identifier at POS or NONE."
   (let* ((file (position-file pos))
-     (row  (position-row  pos))
-     (col  (position-col  pos)))
+         (row  (position-row  pos))
+         (col  (position-col  pos)))
     (liquid-annot-get file row col)))
 
 (defun liquid-annot-at-pos (pos)
@@ -287,7 +293,7 @@
   "Update liquid-annot-table by reloading annot file for buffer in MODE."
   (interactive)
   (let* ((pos  (liquid-get-position))
-     (file (position-file pos)))
+         (file (position-file pos)))
     (liquid-annot-set file mode)))
 
 
@@ -300,31 +306,33 @@
 
 
 
-(defun liquid-tip-init ()
+(defun liquid-tip-init (&optional args)
+  "Initialize/uninitialize liquid-tip when the minor mode is toggled."
+  (let ((liquid-button-lock-off nil))
 
-  (defun liquid-tip-set ()
-    ;; turn on button-lock if it isn't already on
-    (when (not button-lock-mode)
-      (button-lock-mode 1)
-      (setq liquid-button-lock-off 1))
-    (setq liquid-button
-          (button-lock-set-button liquid-id-regexp
-                                  'liquid-tip-show
-                                  :mouse-face nil
-                                  :face nil
-                                  :face-policy nil
-                                  :mouse-binding liquid-tip-trigger)))
+    (defun liquid-tip-set ()
+      ;; turn on button-lock if it isn't already on
+      (when (not button-lock-mode)
+        (button-lock-mode 1)
+        (setq liquid-button-lock-off 1))
+      (setq liquid-button
+            (button-lock-set-button liquid-id-regexp
+                                    'liquid-tip-show
+                                    :mouse-face nil
+                                    :face nil
+                                    :face-policy nil
+                                    :mouse-binding liquid-tip-trigger))
 
-  (defun liquid-tip-unset ()
-    ;; turn off button-lock if we turned it on
-    (when liquid-button-lock-off
-      (button-lock-mode 0)
-      (setq liquid-button-lock-off nil))
-    (button-lock-unset-button liquid-button))
+      (defun liquid-tip-unset ()
+        ;; turn off button-lock if we turned it on
+        (when liquid-button-lock-off
+          (button-lock-mode 0)
+          (setq liquid-button-lock-off nil))
+        (button-lock-unset-button liquid-button))
 
-  (if liquid-tip-mode
-      (liquid-tip-set)
-    (liquid-tip-unset)))
+      (if liquid-tip-mode
+          (liquid-tip-set)
+        (liquid-tip-unset)))))
 
 ;; Reload annotations after check
 ;;;###autoload
